e3a60276ba806aa6dfbe5ca303ca1461e6bd3434
Stranger4uu <yashsaini4824@gmail.com>
2025-12-09 00:55:17 +0530

    added 2 new files

---
+++ area_of_circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float r,area;
+    printf("Enter radius of circle : ");
+    scanf("%f",&r);
+    area = 3.14*r*r;
+    printf("Area of circle is : %f\n",area);
+
+    return 0;
+}
---
+++ circumference_of_circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float r,circumference;
+    printf("Enter radius of circle : ");
+    scanf("%f",&r);
+    circumference = 2*3.14*r;
+    printf("Circumference of circle is : %f\n",circumference);
+
+    return 0;
+}
