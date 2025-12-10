ee52279b730c7215539ccb53e25a0f77246b2a31
Stranger4uu <yashsaini4824@gmail.com>
2025-12-10 13:38:09 +0530

    updated git ignore and added program to check if year entered by user is leap year or not

---
+++ leap_year_or_not.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a ;
+    printf("Enter a year :");
+    scanf("%d",&a);
+
+    if (a % 400 == 0)
+    printf("leap year");
+else if (a % 100 == 0)
+    printf("not leap year");
+else if (a % 4 == 0)
+    printf("leap year");
+else
+    printf("not leap year");
+
+    return 0;
+}
