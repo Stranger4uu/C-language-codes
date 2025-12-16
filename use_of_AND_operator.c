1a5e90ab4490ef6b0e8fcbec3d6709fcd4d2aad1
Stranger4uu <yashsaini4824@gmail.com>
2025-12-16 18:14:07 +0530

    updated git ignore and also added a new file

---
+++ use_of_AND_operator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+int a , b, c;
+
+a=10;
+b=20;
+c=15;
+
+if(b>a && b>a){  // use of AND logical operator
+    printf("B is greater");
+}
+else{printf("B is not greater than a or c ");
+}
+
+
+
+    return 0;
+}
